137aa0a4d3babe691fb9d49f74a2b8954002be34
Remag29 <73289897+Remag29@users.noreply.github.com>
2024-08-18 11:56:48 +0200

    Remove install.sh from Starship installation Add light blue style to title

--- script.sh
+++ script.sh
@@ -1,26 +1,30 @@
 #! /bin/bash
 
-echo "Starting installation of the environment !"
+# Difine colors
+LIGHTBLUE='\033[1;34m'
+NC='\033[0m' # No Color
+
+echo -e "${LIGHTBLUE}Starting installation of the environment !${NC}"
 
 # Update the system
 sudo apt update -q
 
 # Install the necessary packages
-echo "Installing the necessary packages"
+echo "${LIGHTBLUE}Installing the necessary packages${NC}"
 sudo apt install -q -y unzip wget curl git
 
 # Install zsh
-echo "Installing zsh"
+echo "${LIGHTBLUE}Installing zsh${NC}"
 sudo apt install -q -y zsh
 echo "Setting zsh as the default shell"
 chsh -s $(which zsh)
 
 # Install Oh My Zsh
-echo "Installing Oh My Zsh"
+echo "${LIGHTBLUE}Installing Oh My Zsh${NC}"
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
 # Install Nerd Fonts Jetbrains Mono
-echo "Installing Nerd Fonts Jetbrains Mono"
+echo "${LIGHTBLUE}Installing Nerd Fonts Jetbrains Mono${NC}"
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.zip -q
 sudo unzip JetBrainsMono.zip -d /usr/share/fonts
 sudo rm /usr/share/fonts/readme.md
@@ -29,25 +33,26 @@ rm JetBrainsMono.zip
 fc-cache -f -v
 
 # Install Starship
-echo "Installing Starship"
+echo "${LIGHTBLUE}Installing Starship${NC}"
 curl -fsSL -o install.sh https://starship.rs/install.sh
 sh install.sh -y
+rm install.sh
 
 # Copy Starship configuration file
-echo "Copying Starship configuration file"
+echo "${LIGHTBLUE}Copying Starship configuration file${NC}"
 mkdir -p ~/.config
 cp starship.toml ~/.config/starship.toml
 
 # Edit the .zshrc file
-echo "Editing the .zshrc file"
+echo "${LIGHTBLUE}Editing the .zshrc file${NC}"
 echo "eval $(starship init zsh)" >> ~/.zshrc
 
 # Install lsdeluxe
-echo "Installing lsdeluxe"
+echo "${LIGHTBLUE}Installing lsdeluxe${NC}"
 sudo apt install -y lsd
 
 # Install Zsh plugins
-echo "Installing Zsh plugins"
+echo "${LIGHTBLUE}Installing Zsh plugins${NC}"
 # Zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # Zsh-syntax-highlighting
@@ -56,12 +61,12 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.
 git clone https://github.com/fdellwing/zsh-bat ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-bat
 
 # Edit the .zshrc file
-echo "Editing the .zshrc file"
+echo "${LIGHTBLUE}Editing the .zshrc file${NC}"
 plugins="plugins=(git zsh-autosuggestions zsh-syntax-highlighting sudo docker docker-compose z colored-man-pages)"
 sed -i.bak '/^plugins=(/c\'"$plugins" ~/.zshrc
 
 # Copy aliases file
-echo "Copying aliases file"
+echo "${LIGHTBLUE}Copying aliases file${NC}"
 cp .aliases ~/.aliases
 echo "" >> ~/.zshrc
 echo "# Load aliases" >> ~/.zshrc
